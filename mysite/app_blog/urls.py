b944b829f33753d5d68c3874679e8cb44a4b7f51
SashaMaksymiuk <maksymiuk.oleksandr.i@chnu.edu.ua>
2025-10-20 20:25:17 +0300

    Змінено моделі, адмінку і налаштовано media

--- mysite/app_blog/urls.py
+++ mysite/app_blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
