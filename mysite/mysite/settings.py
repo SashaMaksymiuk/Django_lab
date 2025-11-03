78217e226b715739b4f1b0de2263779b518cdd80
SashaMaksymiuk <maksymiuk.oleksandr.i@chnu.edu.ua>
2025-11-03 21:28:47 +0200

    Finish Lab 5

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -114,9 +114,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite', 'static'),
+    os.path.join(BASE_DIR, 'app_blog', 'static'),
+]
+
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
