00564e800448166e0e9be9bd0a09a0358cda2847
SashaMaksymiuk <maksymiuk.oleksandr.i@chnu.edu.ua>
2025-11-03 18:43:12 +0200

    Finish Lab 4: templates, gallery, tests

--- mysite/app_blog/tests_urls.py
+++ mysite/app_blog/tests_urls.py
@@ -2,17 +2,23 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Category, Article
 
-class SimpleTests(TestCase):
-
-    def setUp(self):
+class SimpleViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
         cat = Category.objects.create(category="ТестКат", slug="testkat")
         Article.objects.create(
             title="Тестова стаття", description="Опис", slug="teststat",
-            category=cat, main_page=True
+            category=cat, main_page=True,
+            pub_date="2025-10-20 16:00:00"
         )
 
     def test_home_status(self):
         url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_articles_list_url(self):
+        url = reverse('articles-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
@@ -21,6 +27,12 @@ class SimpleTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_absolute_url_category(self):
-        cat = Category.objects.get(slug="testkat")
-        self.assertEqual(cat.get_absolute_url(), reverse('articles-category-list', args=["testkat"]))
+    def test_article_detail_url(self):
+        url = reverse('news-detail', kwargs={
+            "year": "2025",
+            "month": "10",
+            "day": "20",
+            "slug": "teststat"
+        })
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
